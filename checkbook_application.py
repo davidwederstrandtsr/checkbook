88313bec3cb4228930620d5284704659b3c693ec
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-26 13:22:04 -0600

    fixing repo

--- checkbook_application.py
+++ checkbook_application.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Command Line Checkbook Application
 # You will be creating a command line checkbook application that allows users to track their finances with a command line interface.
 
@@ -17,34 +11,100 @@
 # add some credits, exit the application and run it again, you should still see the balance that you 
 #previously created. In order to do this, your application will need to store it's data in a text file. 
 # Consider creating a file where each line in the file represents a single transaction.
-
-
-# In[19]:
-
+import os.path
+from os import path
 
 filename = "checkbook_application_data.txt"
-with open(filename, "w") as f:
-    f.write('100.00')
+user_choice = True
 
+if not path.exists(filename):
+    with open(filename, "w") as f:
+        f.write('0.00')
+    print("Account created, inital balance: $0.00") 
 
-# In[22]:
-
-
-def get_balance():
-    balance = ''
+# get_current_balance retrieves the last 
+def get_current_balance():
     with open(filename, "r") as f:
+        current_balance = 0
         balance = f.readlines()
-    return float(balance[0])
+        for b in balance:
+            current_balance += float(b)
+    return f"Your current balance is: $ {round(float(current_balance), 2)}"
+
+# make a withdraw
+def withdraw(debit):
+    message = ''
+    debit = float(debit)
+    current_balance = float(get_current_balance())
+    if debit < current_balance:
+        with open(filename, 'a') as f:
+            f.write(f'\n{-debit}')
+        message = get_current_balance()
+    else:
+        message = -1 
+    return message
+ 
+# make a deposit
+def credit(deposit):
+    message = ''
+    deposit = float(deposit)
+    current_balance = float(get_current_balance())
+    if deposit > 0:
+        with open(filename, "a") as f:
+            f.write(f'\n{deposit}')
+        message = get_current_balance()
+    else: # current not working
+        message = -1
+    return message
 
 
-# In[23]:
+def menu():
+    print('What would you like to do?')
+    print()
+    print('1) view current balance\n\
+    2) record a debit (withdraw)\n\
+    3) record a credit (deposit)\n\
+    4) exit')
+    print(f'You entered {type(prompt)}')
+    return prompt
 
+# start of main
 
-print(get_balance())
+    
 
-
-# In[ ]:
-
-
-
-
+print()
+print("~~~ Welcome to your terminal checkbook!~~~")
+print()
+while user_choice == True:
+    menu()
+    prompt = input(': ')
+    while prompt not in ['1', '2', '3', '4']:
+        print("Not a valid entry!")
+        menu()
+        prompt = input(': ')
+    
+    if prompt == '1':
+        get_current_balance()
+    elif prompt == '2':
+        debit = input("How much is the debit? $")
+        resp = withdraw(debit)
+        while resp == -1:
+            print('You do not have enough in account to cover this withdraw.')
+            debit = input("How much is the debit? $")
+            resp = withdraw(debit)
+        print(resp)
+    elif prompt == '3':
+        deposit = input("How much is the deposit? $")
+        resp = credit(deposit)
+        while resp == -1:
+            print('You enter a dollar amount of less then or equal to 0')
+            deposit = input("How much is the deposit? $")
+            resp = credit(deposit)
+        print(resp)
+    else:
+        user_choice = False
+print("Thanks, have a great day")
+        
+            
+        
+        
